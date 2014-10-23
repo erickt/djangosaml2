b831aae887daa158ed6bfae519da4ec226e94f4e
lgs <none@none>
2014-10-23 13:23:04 -0700

    Changes to support Pysaml2 1.0.X

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'pysaml2==0.4.1',
-        'python-memcached==1.48',
+        'pysaml2',
+        'python-memcached',
         ],
     )
